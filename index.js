aecb8de9d0c7fabf28e03cad85b580d97085f76a
krishnamohan <krishnamohan.py@gmail.com>
2021-03-16 12:47:27 +0530

    Added

---
+++ index.js
@@ -0,0 +1,19 @@
+// var i = 0;
+// function move() {
+//   if (i == 0) {
+//     i = 1;
+//     var elem = document.getElementById("myBar");
+//     var width = 5;
+//     var id = setInterval(frame, 30);
+//     function frame() {
+//       if (width >= 90) {
+//         clearInterval(id);
+//         i = 0;
+//       } else {
+//         width++;
+//         elem.style.width = width + "%";
+//         elem.innerHTML = width  + "%";
+//       }
+//     }
+//   }
+// }
